dc55d6ca1893097f12dd8c77a721d752cb7677d0
khushbu311 <sanganikhushbu.12.ss@gmail.com>
2022-06-08 18:35:49 +0530

    Merge branch 'master' of https://github.com/khushbu311/Scoops_Smiles

--- Javascript/slideshow.js
+++ Javascript/slideshow.js
@@ -1,7 +1,7 @@
 $(function() {
     var pic = $('img:first')
     var images = [
-        "/home/neosoft/Desktop/webasic/Scoops_Smiles/b1.jpg",
+        "https://img.perfumerflavorist.com/files/base/allured/all/image/2021/07/pf.ice_cream_flavors.png?auto=format&fill=solid&fit=fill&h=720&w=1280",
         "/home/neosoft/Desktop/webasic/Scoops_Smiles/b2.jpg",
         "/home/neosoft/Desktop/webasic/Scoops_Smiles/b3.jpg",
 

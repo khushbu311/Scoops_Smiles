b5f9167ffc4ada89d0bab657453102576c38f821
khushbu311 <sanganikhushbu.12.ss@gmail.com>
2022-06-08 15:26:04 +0530

    second commite

--- Javascript/buynow.js
+++ Javascript/buynow.js
@@ -1,4 +1,4 @@
 function buynow() {
-    const action = window.open("/home/neosoft/Desktop/webasic/Scoops_Smiles/new.html", "_top")
+    const action = window.open("file:///home/neosoft/Desktop/webasic/Scoops_Smiles/new.html", "_top")
     console.log(action);
 }

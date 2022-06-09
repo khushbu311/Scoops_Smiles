22766b63450236c23869a3f72cc00c50ad7b0228
khushbu311 <sanganikhushbu.12.ss@gmail.com>
2022-06-09 11:17:32 +0530

    Last commit

--- Javascript/slideshow.js
+++ Javascript/slideshow.js
@@ -2,8 +2,10 @@ $(function() {
     var pic = $('img:first')
     var images = [
         "https://img.perfumerflavorist.com/files/base/allured/all/image/2021/07/pf.ice_cream_flavors.png?auto=format&fill=solid&fit=fill&h=720&w=1280",
-        "/home/neosoft/Desktop/webasic/Scoops_Smiles/b2.jpg",
-        "/home/neosoft/Desktop/webasic/Scoops_Smiles/b3.jpg",
+        "https://images.indianexpress.com/2021/07/ice-cream-pixabay-1200.jpg",
+        "https://assets3.thrillist.com/v1/image/2836428/1200x600/scale;",
+        "https://image.shutterstock.com/image-photo/gourmet-summer-dessert-artisanal-craft-260nw-1647372625.jpg",
+        "https://media.gettyimages.com/photos/chocolate-ice-cream-in-a-glass-cup-picture-id936205852?s=612x612",
 
     ]
     var i = 0;
